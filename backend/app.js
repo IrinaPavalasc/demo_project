460ffda2f37f7342d6df56c68643bbc49d9d8b8a
Irina Pavalasc <irinapavalasc@gmail.com>
2023-09-24 17:15:17 +0300

    places controller

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error')
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use('/api/places', placesRoutes);
+
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route.', 404);
+    return next(error);
+});
 
 app.use((error, req, res, next) => {
     if (res.headerSent) {
@@ -14,4 +22,5 @@ app.use((error, req, res, next) => {
 
     res.status(error.code || 500).json({message: error.message || 'Unknown error!' });
 });
+
 app.listen(5000);
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,49 +1,17 @@
 const express = require('express');
 
-const HttpError = require('../models/http-error');
+const placesControllers = require('../controllers/places-controller');
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famour sky scrapers in the world!',
-        location: {
-            lat: 40.7484474,
-            lng: -73.9871516
-        },
-        address: '20 W 34th St, New York, NY 10001',
-        creator: 'u1'
-    }
-];
+router.get('/:pid', placesControllers.getPlaceById);
 
-router.get('/:pid', (req, res, next) => {
-    const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find(p => {
-        return p.id === placeId;
-    });
+router.get('/user/:uid', placesControllers.getPlaceByUserId);
 
-    if (!place) {
-        return next(new HttpError('Place not found.', 404));
-    }
+router.post('/', placesControllers.createPlace);
 
-    res.json({place});
-});
+router.patch('/:pid', placesControllers.updatePlace);
 
-router.get('/user/:uid', (req, res, next) => {
-    const userId = req.params.uid;
-    const places = DUMMY_PLACES.filter(p => {
-        return p.creator === userId;
-    });
-
-    if (!places.length) {
-        return next(new HttpError('Place not found for provided user id.', 404));
-    }
-
-    res.json({places});
-});
-
-
+router.delete('/:pid', placesControllers.deletePlace);
 
 module.exports = router;
